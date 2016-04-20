f8bb4314b7eeed44477de572d8d85cb9587a6a97
ibowen <berwin.lew@gmail.com>
2016-04-19 17:22:26 -0700

    update server and ui

--- server.R
+++ server.R
@@ -1,7 +1,12 @@
 library(shiny)
+library(leaflet)
+library(RColorBrewer)
+library(scales)
+library(lattice)
+library(dplyr)
 library(C50)
-library(e1071)
-library (nnet)
+library(DT)
+
 # file path for the final data set
 filepath <- "./data/final_data_v3.csv"
 # read files
@@ -13,59 +18,153 @@ if(!exists(filepath)) {
     data_rm4$sentiment <- ifelse(data_rm4$Star_Rating_Current > 4,1,0)
     # transfer sentiment to factor variables
     data_rm4$sentiment <- factor(data_rm4$sentiment, levels = c(0,1), labels = c('Bad','Good'))
-    # subset the need columns for modeling
-    colnames <- c('experience', 'spec_count', 'unemployment_rate', 'averge_income', 'avgerage_physician', 'tier_label', 'mnthly_prm', 'ann_ddctbl', 'copay_max', 'coin_max', 'sentiment')
+    # subset the needed columns for modeling
+    colnames <- c('experience', 'spec_count', 'unemployment_rate', 'average_income', 'average_physician', 'tier_label', 'mnthly_prm', 'ann_ddctbl', 'copay_max', 'coin_max', 'sentiment')
     data_rm4 <- data_rm4[, colnames]
+    
+    # Logistic Regression Modeling
+    logsitic_regression_model = glm(formula = sentiment ~ ., family = binomial(link = 'logit'), data = data_rm4)
+    
+    # subset the state list
+    state_list <- as.list(rownames(table(data$state)))
+    # subset the contract list
+    contract_list <- as.list(rownames(table(data$contract_id)))
+    
+    # colnames to show
+    colnames_show <- c('Rating', 'State', 'County', 'Zip', 'Experience(years)', 'Physician Speciaties', 'Unemployment(%)', 'Avg Income(,000$)', 
+                       'Citizens/Physician', 'Tiers', 'Monthy Premium($)', 'Annualy Deductible($)', 'Max Copayment(%)', 'Max Coinsurance(%)')
 }
 
+# Leaflet bindings are a bit slow; for now we'll just sample to compensate
+set.seed(100)
+# By ordering by centile, we ensure that the (comparatively rare) SuperZIPs
+# will be drawn last and thus be easier to see
+# data <- data[order(data$polulation),]
 
-shinyServer(
-    function(input, output){
-        # internal predictors
-        output$internal <- renderUI({
-            tier <- paste("Tier Number: ", input$tier)
-            premium <- paste("Monthy Premium($): ", input$premium)
-            deductible <- paste("Annualy Deductible($): ", input$deductible)
-            copayment <- paste("Maximium Copayment Rate(%): ", input$copayment)
-            coinsurance <- paste("Maximium Coinsurance Rate(%): ", input$coinsurance)
-            HTML(paste(tier, premium, deductible, copayment, coinsurance, sep = '<br/>'))
-        })
-        # external predictors
-        output$external <- renderUI({
-            experience <- paste("Average Work Experience(years): ", input$experience)
-            unemployment <- paste("Unemployment Rate(%): ", input$unemployment)
-            income <- paste("Average Income(,000$): ", input$income)
-            physician <- paste("Average Citizens per Physician: ", input$physician)
-            spec_type <- paste("Physician Speciaty Types in County: ", input$spec_type)
-            HTML(paste(experience, unemployment, income, physician, spec_type, sep = '<br/>'))
-        })
-        output$ratings <- renderUI({
-            predictors <- data.frame(input$experience, input$spec_type, input$unemployment, input$income, input$physician, input$tier, input$premium, input$deductible, input$copayment, input$coinsurance, 0)
-            colnames(predictors) <- colnames
-            # Logistic Regression Modeling
-            logsitic_regression_model = glm(formula = sentiment ~ ., family = binomial(link = 'logit'), data = data_rm4)
-            lr_pred <- predict(logsitic_regression_model, newdata = predictors, type = "response")
-            #lr_rating <- paste("The Logistic Regression Probability: ", round(lr_pred, 2))
-            lr_rating <- paste("The Logistic Regression Probability: ", ifelse(lr_pred > 0.5, 'Good', 'Bad'))
+shinyServer(function(input, output, session) {
+  
+  ## Interactive Map ###########################################
 
-            # Decision Tree Modeling
-            decision_tree_model <- C5.0.default(x = data_rm4[,colnames], y = data_rm4$sentiment)
-            dt_pred <- predict(decision_tree_model, newdata = predictors)
-            dt_rating <- paste("The Decision Tree Prediction: ", dt_pred)
+    # Create the map
+    output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles(
+        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+      ) %>%
+      setView(lng = -93.85, lat = 37.45, zoom = 4)
+    })
 
-            # Artificial Neural Network Modeling
+    # A reactive expression that returns the set of zips that are
+    # in bounds right now
+    zipsInBounds <- reactive({
+        if (is.null(input$map_bounds))
+          return(data_map[FALSE,])
+        bounds <- input$map_bounds
+        latRng <- range(bounds$north, bounds$south)
+        lngRng <- range(bounds$east, bounds$west)
 
-            neural_network_model <- nnet(formula =  sentiment~.-sentiment, data =data_rm4, decay = 0.5, size = 6)
-            nn_pred <- predict(neural_network_model, newdata = predictors, type='class')
-            nn_rating <- paste("The Neural Network Prediction: ", ifelse(lr_pred > 0.5, 'Good', 'Bad'))
-
-            # Naive Bayes Modeling
-            naive_bayes_model <- naiveBayes(sentiment~ . , data=data_rm4)
-            nb_pred <- predict(naive_bayes_model, newdata = predictors, type = 'class')
-            nb_rating <- paste("The Naive Bayes Prediction: ", nb_pred)
+        subset(data_map,
+               latitude >= latRng[1] & latitude <= latRng[2] &
+                 longitude >= lngRng[1] & longitude <= lngRng[2])
+    })
 
 
-            HTML(paste(lr_rating, dt_rating, nb_rating, nn_rating, sep = '<br/>'))
-        })
+    # This observer is responsible for maintaining the circles and legend,
+    # according to the variables the user has chosen to map to color and size.
+    observe({
+        colorBy <- input$color
+
+    if (colorBy == "average_physician") {
+        #Color and palette are treated specially in the "superzip" case, because
+        #the values are categorical instead of continuous.
+        colorData <- ifelse(data$average_physician <= 10, "0-20", ifelse(data$average_physician<=20, "10-20", ifelse(data$average_physician <= 50, "20-50", "50+")))
+        pal <- colorFactor("Spectral", colorData)
+        radius <- data[[colorBy]] / max(data[[colorBy]]) * 50000
+    } else {
+        colorData <- data[[colorBy]]
+        pal <- colorBin("Spectral", colorData, 7, pretty = FALSE)
+        radius <- data[[colorBy]] / max(data[[colorBy]]) * 20000
+     }
+
+
+    leafletProxy("map", data = data) %>%
+      clearShapes() %>%
+      addCircles(~longitude, ~latitude, radius=radius, layerId= ~zip_code,
+                 stroke=FALSE, fillOpacity=0.4, fillColor=pal(colorData)) %>%
+      addLegend("bottomleft", pal=pal, values = colorData, title=colorBy,
+                layerId="colorLegend")
+    })
+
+    # Show a popup at the given location
+    showZipcodePopup <- function(zipcode, lat, lng) {
+    selectedZip <- data[data$zip_code == zipcode,]
+    content <- as.character(tagList(
+      tags$h4("Monthly Premium($):", as.integer(selectedZip$mnthly_prm)),
+      tags$strong(HTML(sprintf("%s", selectedZip$zip_code))),
+      tags$br(),
+      sprintf("Average Household Income(,000$): %s", dollar(selectedZip$average_income)),
+      tags$br(),
+      sprintf("Average Work Experience for Physicians: %s", as.integer(selectedZip$experience)),
+      tags$br(),
+      sprintf("Average Citizens per Physician: %s", selectedZip$average_physician)
+    ))
+    leafletProxy("map") %>% addPopups(lng, lat, content, layerId = zip_code)
     }
-)
+
+    # When map is clicked, show a popup with city info
+    observe({
+    leafletProxy("map") %>% clearPopups()
+    event <- input$map_shape_click
+    if (is.null(event))
+      return()
+
+    isolate({
+      showZipcodePopup(event$id, event$lat, event$lng)
+    })
+    })
+  
+    # Preidction Part #######################
+    # External Predictors with Geographic Info
+    data_external <- reactive({
+        unique(data[data$state == input$state, c('state', 'county', 'zip_code', 'experience', 'spec_count', 'unemployment_rate', 'average_income', 'average_physician')])
+    })
+    # Internal Predictors
+    data_internal <- reactive({
+        data_internal <- c(input$tier, input$premium, input$deductible, input$copayment, input$coinsurance)
+        t(replicate(nrow(data_external()), data_internal))
+    })
+    # Ratings
+    data_rating <- reactive({
+        data_external <- data_external()[, -c(1,2,3)]
+        predictors <- cbind(data_external, data_internal())
+        predictors$dummy <- 'dummy'
+        colnames(predictors) <- colnames
+        lr_pred <- predict(logsitic_regression_model, newdata = predictors, type = "response")
+        #lr_rating <- paste("The Logistic Regression Probability: ", round(lr_pred, 2))
+        ifelse(lr_pred > 0.5, 'Good', 'Bad')
+    })
+    
+    # Final Data Sent back to client
+    data_show <- reactive({
+        data_show <- cbind(cbind(data_rating(), data_external()), data_internal())
+        rownames(data_show) <- NULL
+        data_show
+    })
+    
+    output$pred_list <- DT::renderDataTable({
+        DT::datatable(data_show(), colnames = colnames_show)
+  })
+
+  # fill states
+  observe({
+      updateSelectInput(session, "state", choices = state_list, selected = state_list[1])
+  })
+  
+  # fill contract id
+  observe({
+      updateSelectInput(session, "contract_id", choices = contract_list, selected = contract_list[1])
+  })
+  
+})
+
--- ui.R
+++ ui.R
@@ -1,41 +1,71 @@
 library(shiny)
-shinyUI(fluidPage(
-    # Application title
-    #titlePanel("Preidictors"),
-    # Siderbar Input
-    sidebarLayout(
-        sidebarPanel(
-            h3('External Predictors'),
-            sliderInput('experience', 'Average Work Experience(years)', min = 0, max = 40, step = 1, value = 10),
-            sliderInput('unemployment', 'Unemployment Rate(%)', min = 0, max = 100, step = 1, value = 5),
-            sliderInput('income', 'Average Income(,000$)', min = 0, max = 100000, step = 5, value = 30),
-            sliderInput('physician', 'Average Citizens per Physician', min = 0, max = 1000, step = 1, value = 50),
-            sliderInput('spec_type', 'Physician Speciaty Types in County', min = 0, max = 100, step = 1, value = 20),
-            h3('Internal Predictors'),
-            sliderInput('tier', 'Tier Number', min = 2, max = 10, step = 1, value = 5),
-            sliderInput('premium', 'Monthy Premium($)', min = 0, max = 100, step = 1, value = 20),
-            sliderInput('deductible', 'Annualy Deductible($)', min = 0, max = 500, step = 10, value = 100),
-            sliderInput('copayment', 'Maximium Copayment Rate(%)', min = 0, max = 100, step = 1, value = 20),
-            sliderInput('coinsurance', 'Maximium Coinsurance Rate(%)', min = 0, max = 50, step = 1, value = 25),
-            submitButton("submit")
-        ),
-    
-        mainPanel(
-            h1('Drug and Health Plan Design Panel'),
-            helpText("This application is for predicting the customer ratings for the drug and Health Plan"),
-            br(),
-            
-            h3('Internal Predictors:'),
-            htmlOutput("internal"),
-            br(),
-            
-            h3('External Predictors:'),
-            htmlOutput("external"),
-            br(),
-            
-            h3('Prediction Ratings:'),
-            h3(htmlOutput("ratings"))
-        )
-    )
-    )
+library(leaflet)
+library(DT)
+
+# Choices for drop-downs
+vars <- c(
+  "Monthly Premium" = "mnthly_prm",
+  "Annual Deductible" = "ann_ddctbl",
+  "Average Household Income(,000$)" = "average_income",
+  "Average Citizens per Physician" = "average_physician",
+  "Unemployment Rate(%)" = "unemployment_rate",
+  "Physician Specialties" = "spec_count"
 )
+
+
+shinyUI(navbarPage("Durg and Health Plan Test Panel", id="nav",
+    tabPanel("Interactive map",
+        div(class="outer",
+            tags$head(
+              # Include our custom CSS
+              includeCSS("styles.css"),
+              includeScript("gomap.js")
+            ),
+            leafletOutput("map", width="100%", height="100%"),
+
+            # Shiny versions prior to 0.11 should use class="modal" instead.
+            absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                          draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                          width = 330, height = "auto",
+
+                          h2("ZIP explorer"),
+
+                          selectInput("color", "Group By", vars)
+                          )
+            ),
+            tags$div(id="cite",
+                     'Data compiled for ', tags$em('Coming Apart: The Medicare Data, 2014'), ' by 510 team-2'
+            )
+    ),
+                tabPanel("Prediction Panel", 
+                    fluidPage(
+                        # Application title
+                        #titlePanel("Preidictors"),
+                        # Siderbar Input
+                        sidebarLayout(
+                            sidebarPanel(
+                                # Select State and Contract
+                                selectInput(inputId = 'state', label   = 'Choose State:', choices = "AL"),
+                                selectInput(inputId = 'contract_id', label   = 'Choose Contract:', choices = ""),
+                                
+                                h3('Internal Predictors'),
+                                sliderInput('tier', 'Tier Number', min = 2, max = 10, step = 1, value = 5),
+                                sliderInput('premium', 'Monthy Premium($)', min = 0, max = 100, step = 1, value = 20),
+                                sliderInput('deductible', 'Annualy Deductible($)', min = 0, max = 500, step = 10, value = 100),
+                                sliderInput('copayment', 'Maximium Copayment Rate(%)', min = 0, max = 100, step = 1, value = 20),
+                                sliderInput('coinsurance', 'Maximium Coinsurance Rate(%)', min = 0, max = 50, step = 1, value = 25),
+                                submitButton("submit")
+                            ),
+                            
+                            mainPanel(
+                                h1('Drug and Health Plan Design Panel'),
+                                helpText("This application is for predicting the customer ratings for the drug and Health Plan"),
+                                br(),
+                                
+                                DT::dataTableOutput('pred_list')
+                            
+                            )
+                        )
+                    )
+                )
+))
